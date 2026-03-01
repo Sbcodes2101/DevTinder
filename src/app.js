2a3ef9a14ead2499f12f702f329afbeb2598eb88
Sarthak6972 <140512395+Sarthak6972@users.noreply.github.com>
2026-03-01 19:31:27 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use("/test",(req,res)=>{
+    res.send("Hello from the server!");
+}) 
+app.use("/hello",(req,res)=>{
+    res.send("HELLO HELLO HELLO");
+})
+app.listen(7777,()=>{
+    console.log("Server is listening on port 3000");
+})
