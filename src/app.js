6ece31327d94733180828dd359fe4fa9b84fa472
Sarthak6972 <140512395+Sarthak6972@users.noreply.github.com>
2026-03-02 18:53:23 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+
+// This will only handle GET call to /user
+app.get("/user",(req,res) => {
+    res.send({firstName :"Sarthak", lastName: "Bahuguna"})
+})
+
+app.post("/user",async (req,res)=>{
+    console.log(req.body)
+    res.send("Save data to the database!");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully!")
+})
+
 app.use("/test",(req,res)=>{
     res.send("Hello from the server!");
 }) 
-app.use("/hello",(req,res)=>{
-    res.send("HELLO HELLO HELLO");
-})
+
 app.listen(7777,()=>{
-    console.log("Server is listening on port 3000");
+    console.log("Server is listening on port 7777");
 })
