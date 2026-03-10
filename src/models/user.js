972f084eb80a22125af1820f151d3f5faf9582d3
Sarthak6972 <140512395+Sarthak6972@users.noreply.github.com>
2026-03-10 19:09:18 +0530

    Schema Validations

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,47 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true,
+        trim:true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number 
+        type: Number ,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("gender daa is not valid");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+    },
+    about: {
+        type: String,
+        default: "This is a default about of the user!"
+    },
+    skills: {
+        type: [String],
     }
+},{
+    timestamps: true
 });
 
 const userModel = mongoose.model("User",userSchema);
